d038ea70f4d3c62fbdcc8b63bcb7fd5ebe12a866
cbdeassi <cbdeassi@scspc711.cs.uwaterloo.ca>
2017-10-23 14:19:45 -0400

    Initial commit. Created ArchetypeCatalogHandler to handle archetype-catalog.xml files.

---
+++ MavenCrawler/src/ca/uwaterloo/swag/mavencrawler/ArchetypeCatalogHandler.java
@@ -0,0 +1,87 @@
+package ca.uwaterloo.swag.mavencrawler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * XML handler to handle archetype-catalog.xml file from the root of a Maven repository.
+ * 
+ * @author cbdeassi
+ */
+public class ArchetypeCatalogHandler extends DefaultHandler {
+	
+	enum ElementType {
+		ARCHETYPE,
+		GROUPID,
+		ARTIFACTID,
+		VERSION,
+		DESCRIPTION,
+		NONE
+	}
+
+	private ElementType currentElementType = ElementType.NONE;
+	private Archetype currentArchetype = null;
+	private List<Archetype> archetypes = new ArrayList<Archetype>();
+	
+	public List<Archetype> getArchetypes() {
+		return archetypes;
+	}
+	
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		super.startElement(uri, localName, qName, attributes);
+
+		try {
+			currentElementType = ElementType.valueOf(qName.toUpperCase());
+		} catch (Exception e) {
+			// If an unknown element is found
+			currentElementType = ElementType.NONE;
+		}
+		
+		if (currentElementType == ElementType.ARCHETYPE) {
+			currentArchetype = new Archetype();
+			this.archetypes.add(currentArchetype);
+		}
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		super.endElement(uri, localName, qName);
+		currentElementType = ElementType.NONE;
+	}
+	
+	@Override
+	// Reading data from inside an element
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		super.characters(ch, start, length);
+		
+		String value = new String(ch, start, length);
+		
+		switch (currentElementType) {
+		case GROUPID:
+			currentArchetype.setGroupId(value);
+			break;
+		case ARTIFACTID:
+			currentArchetype.setArtifactId(value);
+			break;
+		case VERSION:
+			currentArchetype.setVersion(value);
+			break;
+		case DESCRIPTION:
+			currentArchetype.setDescription(value);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	@Override
+	public void endDocument() throws SAXException {
+		super.endDocument();
+	}
+
+}
---
+++ MavenCrawler/test/ca/uwaterloo/swag/mavencrawler/ArchetypeCatalogHandlerTest.java
@@ -0,0 +1,64 @@
+package ca.uwaterloo.swag.mavencrawler;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class ArchetypeCatalogHandlerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws ParserConfigurationException, SAXException, IOException {
+		
+		// Given
+		InputStream stream = this.getClass().getResourceAsStream("archetype-catalog-example.xml");
+		SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+		ArchetypeCatalogHandler handler = new ArchetypeCatalogHandler();
+		
+		// When
+		parser.parse(stream, handler);
+		
+		// Then
+		assertEquals(2, handler.getArchetypes().size());
+		
+		Archetype archetype = handler.getArchetypes().get(0);
+		assertEquals("am.ik.archetype", archetype.getGroupId());
+		assertEquals("maven-reactjs-blank-archetype", archetype.getArtifactId());
+		assertEquals("1.0.0", archetype.getVersion());
+		assertEquals("Blank Project for React.js", archetype.getDescription());
+		
+		archetype = handler.getArchetypes().get(1);
+		assertEquals("us.fatehi", archetype.getGroupId());
+		assertEquals("schemacrawler-archetype-plugin-lint", archetype.getArtifactId());
+		assertEquals("11.02.01", archetype.getVersion());
+		assertNull(archetype.getDescription());
+	}
+
+}
