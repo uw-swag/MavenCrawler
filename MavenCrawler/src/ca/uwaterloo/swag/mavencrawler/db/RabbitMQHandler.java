a865d02fafe5873038d87c99024e3c5ead69b5ea
Cassiano Monteiro <cassianomonteiro@gmail.com>
2018-05-11 16:24:08 -0400

    Created RabbitMQ handler.

---
+++ MavenCrawler/src/ca/uwaterloo/swag/mavencrawler/db/RabbitMQHandler.java
@@ -0,0 +1,197 @@
+package ca.uwaterloo.swag.mavencrawler.db;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import ca.uwaterloo.swag.mavencrawler.helpers.LoggerHelper;
+
+public class RabbitMQHandler {
+	
+	private String username;
+	private String password;
+	private String host;
+	private int port;
+	private String queueName;
+	
+	private Logger logger;
+	private Connection connection;
+	private Channel channel;
+	
+	private enum PropertyType {
+		RABBITMQ_HOST,
+		RABBITMQ_PORT,
+		RABBITMQ_USERNAME,
+		RABBITMQ_PASSWORD,
+		RABBITMQ_QUEUE
+	}
+
+	// Disable default constructor
+	private RabbitMQHandler() {}
+	
+	public static RabbitMQHandler newInstance(Logger logger) {
+		RabbitMQHandler handler = new RabbitMQHandler();
+		handler.logger = logger;
+		
+		return handler;
+	}
+
+	public static RabbitMQHandler newInstance(Logger logger, Properties properties) {
+		RabbitMQHandler handler = new RabbitMQHandler();
+		handler.logger = logger;
+		handler.host = properties.getProperty(PropertyType.RABBITMQ_HOST.name());
+		handler.port = Integer.valueOf(properties.getProperty(PropertyType.RABBITMQ_PORT.name()));
+		handler.username = properties.getProperty(PropertyType.RABBITMQ_USERNAME.name());
+		handler.password = properties.getProperty(PropertyType.RABBITMQ_PASSWORD.name());
+		handler.queueName = properties.getProperty(PropertyType.RABBITMQ_QUEUE.name());
+		
+		return handler;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
+	}
+
+	public void sendMessage(String message) {
+		
+		if (connect()) {
+			try {
+				channel.basicPublish("", queueName, null, message.getBytes());
+			} catch (IOException e) {
+				LoggerHelper.logError(logger, e, "Could not send message to RabbitMQ.");
+			}
+		}
+		else {
+			LoggerHelper.log(logger, Level.SEVERE, "Could not connect to RabbitMQ.");
+		}
+		
+	}
+	
+	public static interface MessageHandler {
+		public boolean handleMessage(String message);
+	}
+	
+	public void listenMessages(MessageHandler messageHandler) {
+
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope,
+					AMQP.BasicProperties properties, byte[] body)
+							throws IOException {
+				
+				String message = new String(body, "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+				
+				if (messageHandler.handleMessage(message)) {
+					this.getChannel().basicAck(envelope.getDeliveryTag(), false);
+				}
+				else {
+					this.getChannel().basicReject(envelope.getDeliveryTag(), true);
+				}
+				
+			}
+		};
+		
+		try {
+			channel.basicConsume(queueName, false, consumer);
+		} catch (IOException e) {
+			LoggerHelper.logError(logger, e, "Could not listen to messages from RabbitMQ.");
+		}
+		
+	}
+	
+	public boolean isConnected() {
+		return (channel != null && channel.isOpen());
+	}
+
+	public boolean connect() {
+		
+		if (isConnected()) return true;
+
+		boolean success = false;
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUsername(getUsername());
+		factory.setPassword(getPassword());
+		factory.setHost(getHost());
+		factory.setPort(getPort());
+		
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(queueName, true, false, false, null);
+			success = channel.isOpen();
+		} catch (IOException|TimeoutException e) {
+			LoggerHelper.logError(logger, e, "Could not connect to RabbitMQ.");
+			channel = null;
+			connection = null;
+		}
+
+		return success;
+	}
+
+	public boolean disconnect() {
+
+		boolean success = false;
+		
+		try {
+			channel.close();
+			connection.close();
+			success = (!channel.isOpen() && !connection.isOpen());
+		} catch (IOException | TimeoutException e) {
+			LoggerHelper.logError(logger, e, "Could not disconnect to RabbitMQ.");
+		} finally {
+			channel = null;
+			connection = null;
+		}
+		
+		return success;
+	}
+
+}
---
+++ MavenCrawler/test/ca/uwaterloo/swag/mavencrawler/db/RabbitMQHandlerTest.java
@@ -0,0 +1,130 @@
+package ca.uwaterloo.swag.mavencrawler.db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.uwaterloo.swag.mavencrawler.db.RabbitMQHandler.MessageHandler;
+
+public class RabbitMQHandlerTest {
+	
+	private RabbitMQHandler handler;
+	private boolean messageConfirmation;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * TODO: use mock instead of actual server
+	 */
+	@Before
+	public void setUp() throws Exception {
+		handler = RabbitMQHandler.newInstance(Logger.getLogger(RabbitMQHandlerTest.class.getName()));
+		handler.setHost("localhost");
+		handler.setPort(5672);
+		handler.setUsername("guest");
+		handler.setPassword("guest");
+		handler.setQueueName("test_queue");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testFactoryMethodWithProperties() throws IOException {
+		
+		// Given
+		Logger aLogger = Logger.getLogger(this.getClass().getName());
+		Properties properties = new Properties();
+		properties.load(this.getClass().getResourceAsStream("../crawler-config-example.conf"));
+		
+		// When
+		RabbitMQHandler handler = RabbitMQHandler.newInstance(aLogger, properties);
+		
+		// Then
+		assertNotNull(handler);
+		assertEquals("127.0.0.1", handler.getHost());
+		assertEquals(10000, handler.getPort());
+		assertEquals("rabbitmq_username", handler.getUsername());
+		assertEquals("rabbitmq_password", handler.getPassword());
+		assertEquals("queue", handler.getQueueName());
+	}
+	
+	/**
+	 * Uncomment @Test to run it on an actual server.
+	 */
+//	@Test
+	public void testConnect() {
+
+		// Given
+		// initial parameters
+		
+		// When
+		assertTrue(handler.connect());
+		
+		// Then
+		assertTrue(handler.isConnected());
+	}
+
+	/**
+	 * Uncomment @Test to run it on an actual server.
+	 */
+//	@Test
+	public void testDisconnect() {
+
+		// Given
+		assertTrue(handler.connect());
+		
+		// When
+		assertTrue(handler.disconnect());
+		
+		// Then
+		assertFalse(handler.isConnected());
+	}
+
+	/**
+	 * Uncomment @Test to run it on an actual server.
+	 */
+//	@Test
+	public void testMessaging() throws InterruptedException {
+		
+		// Given
+		String sentMessage = "testMessage";
+		messageConfirmation = false;
+		
+		MessageHandler messageHandler = new MessageHandler() {
+			@Override
+			public boolean handleMessage(String receivedMessage) {
+				assertEquals(sentMessage, receivedMessage);
+				messageConfirmation = receivedMessage.equals(sentMessage);
+				return messageConfirmation;
+			}; 
+		};
+		
+		// When
+		handler.sendMessage(sentMessage);
+		handler.listenMessages(messageHandler);
+		Thread.sleep(200);
+		
+		// Then
+		assertTrue(messageConfirmation);
+	}
+
+}
